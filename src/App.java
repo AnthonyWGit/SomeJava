e33e6555ccae002c223e03d3411ac7ceac5048f9
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-11-05 08:03:07 +0100

    using hex val to change bg color

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class App {
     public static void main(String[] args) throws Exception 
@@ -9,7 +11,13 @@ public class App {
             frame.setSize(500,500);
             frame.setTitle("Main");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //we need this so the program close properly 
+            frame.setResizable(false); // Do not allow resize 
             frame.setVisible(true);
+
+            ImageIcon image = new ImageIcon("logo.png");
+
+            frame.setIconImage(image.getImage());
+            frame.getContentPane().setBackground(new Color(0x402f3c)); //change default color background | rgb or hex hex is 0xF000000 format
         }
     }
 }
