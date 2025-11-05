9593c0f137a5531d5b0e68d64b64971225525e13
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-11-05 08:35:18 +0100

    creating a label for my gui

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import java.awt.Color;
 
 public class App {
@@ -12,12 +14,20 @@ public class App {
             frame.setTitle("Main");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //we need this so the program close properly 
             frame.setResizable(false); // Do not allow resize 
-            frame.setVisible(true);
+
 
             ImageIcon image = new ImageIcon("logo.png");
 
             frame.setIconImage(image.getImage());
             frame.getContentPane().setBackground(new Color(0x402f3c)); //change default color background | rgb or hex hex is 0xF000000 format
+
+            JLabel jlabel = new JLabel();
+            jlabel.setText("This is a label");
+            jlabel.setForeground(Color.white);;
+            
+            frame.add(jlabel);
+
+            frame.setVisible(true);
         }
     }
 }
