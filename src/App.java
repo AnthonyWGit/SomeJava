8ddc6fb56b879fbda167728620b181b1a38f5a41
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-11-06 10:12:16 +0100

    using custom font

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
 
 public class App {
     public static void main(String[] args) throws Exception 
@@ -22,7 +24,7 @@ public class App {
             frame.getContentPane().setBackground(new Color(0x402f3c)); //change default color background | rgb or hex hex is 0xF000000 format
 
             JLabel jlabel = new JLabel("This is a label", imageLabel, JLabel.CENTER);
-            jlabel.setForeground(Color.white);
+            jlabel.setForeground(Color.white); //color of text 
             jlabel.setHorizontalTextPosition(JLabel.CENTER);
             jlabel.setVerticalTextPosition(JLabel.BOTTOM); // Put text below image
             jlabel.setHorizontalAlignment(JLabel.CENTER);
@@ -33,7 +35,12 @@ public class App {
             frame.add(jlabel);
 
             frame.setVisible(true);
-            jlabel.setVisible(true);
+            //Custom font handler
+            Font customFont = null;
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("Saira-Italic-VariableFont_wdth,wght.ttf"));
+            customFont = customFont.deriveFont(Font.PLAIN, 40f);
+
+            jlabel.setFont(customFont);
         }
     }
 }
