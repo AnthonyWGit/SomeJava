ad2a440f308a73e8bf63892eded353d4292a3b94
AnthonyWGit <anthony.wetzstein@gmail.com>
2026-02-16 13:31:33 +0100

    using correct GridLayout instead of GridBagLayout

--- src/GridFrame.java
+++ src/GridFrame.java
@@ -7,7 +7,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 public class GridFrame extends JFrame {
     
     GridFrame() { 
@@ -15,14 +15,17 @@ public class GridFrame extends JFrame {
         this.setTitle("Alt frame");
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
         this.setResizable(true);
-        this.setLayout(new GridBagLayout()); //by default everything 
+        this.setLayout(new GridLayout(2,2)); // 2 rows 2 columns
 
         this.add(new JButton("1"));
         this.add(new JButton("2"));
         this.add(new JButton("3"));
         this.add(new JButton("4"));
         this.add(new JButton("5"));
-
+        this.add(new JButton("6"));
+        this.add(new JButton("7"));
+        this.add(new JButton("8"));
+        this.add(new JButton("9"));
         this.setVisible(true);
     }
 }
