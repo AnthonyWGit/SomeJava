17f0d3df8ee58a4017de82a394c61a5b8070c55b
AnthonyWGit <anthony.wetzstein@gmail.com>
2026-02-26 09:15:51 +0100

    add Jpanel to the frame and add a label

--- src/LayeredPanel.java
+++ src/LayeredPanel.java
@@ -5,8 +5,15 @@ public class LayeredPanel extends JFrame
 {
     LayeredPanel()
     {
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.BLUE);
+
         JLayeredPane layeredPanel = new JLayeredPane();
+        layeredPanel.add(label1);
+
         JFrame frame = new JFrame("LayeredPanel");
+        frame.add(layeredPanel);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //DISPOSE ON CLOSE doesn't shutdown the whole app 
         frame.setSize(new Dimension(500,500));
         frame.setLayout(null);
