a5b26d2a8aea4d02311dae6f9c1a85845a676941
AnthonyWGit <anthony.wetzstein@gmail.com>
2026-05-06 09:38:22 +0200

    renaming file

---
+++ src/JOptionPaneO.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class JOptionPaneO extends JFrame {
+    JOptionPaneO()
+    {
+        JOptionPane.showMessageDialog(null, "Lorem Ipsum Dolor sit amet", "title", JOptionPane.PLAIN_MESSAGE);
+    }
+}
